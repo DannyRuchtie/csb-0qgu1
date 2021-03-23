e45f5b653d30c6342a35a13a34868b88dd131275
Code Sandbox <hello@codesandbox.io>
2021-03-23 11:55:15 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.99a846efb4a1d9f6f298789f35f73a17.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e10fcc0b6a538626708de935866d962",
+    "url": "https://DannyRuchtie.github.io/csb-0qgu1/index.html"
+  },
+  {
+    "revision": "4270fcf29bd27e9b0d2f",
+    "url": "https://DannyRuchtie.github.io/csb-0qgu1/static/js/2.5231307b.chunk.js"
+  },
+  {
+    "revision": "4f569b4f4e48e89be67a",
+    "url": "https://DannyRuchtie.github.io/csb-0qgu1/static/js/main.7b0a5611.chunk.js"
+  },
+  {
+    "revision": "542d292908246573c6b2",
+    "url": "https://DannyRuchtie.github.io/csb-0qgu1/static/js/runtime~main.731e9272.js"
+  }
+]);
